3066020bd199960191b64e2bf4a9ec86f8e686be
GuiSaibert <jotaberg01323@gmail.com>
2023-08-18 20:36:06 -0300

    carai

---
+++ banana.py
@@ -0,0 +1,10 @@
+opcao = int(input("Escolha uma opção:\n1 - Maçã\n2 - Banana\n3 - Laranja "))
+
+if opcao == 1:
+    print("Voce escolheu Maçã. ")
+elif opcao == 2:
+    print("Voce escolheu Banana. ")
+elif opcao == 3:
+    print("Voce escolheu Laranja. ")
+else:
+    print("Opcao invalida")
